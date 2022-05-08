cadccd7344239b6d31fae643575e3d284f5a8718
Muhammad Faisal Pangestu <43786759+codeszhal@users.noreply.github.com>
2022-05-08 13:23:36 +0700

    Add files via upload

--- stack.py
+++ stack.py
@@ -2,11 +2,11 @@
 Stack
 
 You need to make a back button for a browser.
-You use a stack to store the website items visited. Each new link visited is pushed onto the stack.
+You use a stack to store the website links visited. Each new link visited is pushed onto the stack.
 The back button needs to pop the top link from the stack and navigate to it.
 
-The given code declares a Browser class as a stack and implements some of its methods. Then, some items are pushed onto the stack.
-A while loop is then used to go back to all items and print them.
+The given code declares a Browser class as a stack and implements some of its methods. Then, some links are pushed onto the stack.
+A while loop is then used to go back to all links and print them.
 
 Implement the required pop() method for the Browser, so that the given code works as expected.
 '''
@@ -18,8 +18,8 @@ class Browser:
     def is_empty(self):
       return self.items == []
   
-    def push(self, link):
-      self.items.insert(0, link)
+    def push(self, items):
+      self.items.insert(0, items)
     
     def pop(self):
       p = self.items[0]
